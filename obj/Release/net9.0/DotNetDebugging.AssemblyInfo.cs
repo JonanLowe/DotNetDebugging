a22fda0a088502f70849c48c98201eec53fd7cc6
Jon Lowe <jonathan_a_lowe@hotmail.com>
2025-02-28 11:22:09 +0000

    add debug messages, and a debug assertion if output isn't 5

--- obj/Release/net9.0/DotNetDebugging.AssemblyInfo.cs
+++ obj/Release/net9.0/DotNetDebugging.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aaea1e085c5b90c246c1a7c186bd90607803eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
